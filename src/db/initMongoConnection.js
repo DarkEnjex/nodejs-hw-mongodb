792355595e17d52ad4dabe208c386c065d85a62d
Valeri <mylenovo21072017@gmail.com>
2024-12-28 01:07:34 +0200

    do

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+export const initMongoConnection = async () => {
+    try {
+        const user = process.env.MONGODB_USER;
+        const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+        const url = process.env.MONGODB_URL;
+        const dbName = process.env.MONGODB_DB;
+
+        await mongoose.connect(
+            `mongodb+srv://${user}:${password}@${url}/${dbName}?retryWrites=true&w=majority&appName=ClusterHW`,
+        );
+
+        console.log('Mongo connection successfully established!');
+    } catch (error) {
+        console.error('Failed to connect to MongoDB', error);
+        process.exit(1);
+    }
+};
